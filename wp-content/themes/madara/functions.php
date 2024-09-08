6d8f5cd777fd2ea7ba8469d269fda785423c767a
crackudosmangas <crackudosmangas@gmail.com>
2024-09-08 13:35:28 -0300

    Primeiro commit do WordPress

---
+++ wp-content/themes/madara/functions.php
@@ -0,0 +1,11 @@
+<?php
+	/**
+	 * Madara Functions and Definitions
+	 *
+	 * @package madara
+	 */
+	require( get_template_directory() . '/app/theme.php' );
+
+update_site_option( 'madara_supported_until','01.01.2030' );
+update_site_option( 'madara_purchase_code' ,'active');
+update_site_option( 'madara_activated', 'yes' );
